e2d844b7f3930b0bd40710ec2b76d45d867426cf
Kevin Tsai <k.tsai92@gmail.com>
2019-06-28 00:01:03 -0400

    added code

---
+++ demonymApp.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './demonymApp.css'
+
+import Demonym from './demonym';
+import CountrySelector from './countrySelector';
+
+
+class DemonymApp extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			countries: [],
+			selected: null
+		};
+	}
+
+	componentDidMount() {
+    fetch('https://country.register.gov.uk/records.json?page-size=5000')
+      .then(response => response.json())
+      .then(data => {
+        const countries = Object.keys(data)
+              .map(key => data[key].item[0]);
+        this.setState({
+          countries
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="demonym_app">
+        <CountrySelector countries={[{name:"Barbados"}, {name:"Bahrain"}]}/>
+        <Demonym name="Barbadian" country="Barbados"/>
+      </div>
+    );
+  }
+}
+
+export default DemonymApp;
